98ec252df2422e674ca88d1d330555d3515281ed
AnsariKhaoula <91264438+KhaoulaAnsari@users.noreply.github.com>
2025-07-23 15:23:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/KhaoulaAnsari.github.io/",
+  base: "/", // <-- Corrigé ici
   plugins: [react()],
   resolve: {
     alias: {
